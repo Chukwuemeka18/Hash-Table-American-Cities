c75344916a2f86fd3ffa2b416a1dbe622b892b82
Chukwuemeka Elendu <ChukwuemekaE91021@gmail.com>
2020-11-18 11:51:20 +0000

    Added more states

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct US_City{
 node;
 
 int main(void) {
-  printf("Hello America\n");
+  printf("Hello America\n\n");
 
   /*
 
@@ -26,33 +26,150 @@ int main(void) {
 
 
   */
+  
   node* city[26];
 
-  node* Oakland = malloc(sizeof(node));
-  Oakland->name = "Oakland";
-  city[15-1] = Oakland;
+///////////////////////////////////////////////
+  
+  //A
+  node* Austin = malloc(sizeof(node));
+  Austin->name = "Austin";
+  city[1-1] = Austin;
 
-  node* Merced = malloc(sizeof(node));
-  Merced->name = "Merced";
-  city[13-1] = Merced;
+  //B
+  node* Boston = malloc(sizeof(node));
+  Boston->name = "Boston";
+  city[2-1] = Boston;
 
-  node* NewYorkCity = malloc(sizeof(node));
-  NewYorkCity->name = "New York City";
-  city[25-1] = NewYorkCity;
+  //C
+  node* Columbus = malloc(sizeof(node));
+  Columbus->name = "Columbus";
+  city[3-1] = Columbus;
 
-  node* LA = malloc(sizeof(node));
-  LA->name = "LA";
-  city[12-1] = LA;
+  //D
+  node* Denver = malloc(sizeof(node));
+  Denver->name = "Denver";
+  city[4-1] = Denver;
 
-  node* Fork = malloc(sizeof(node));
-  Fork->name = "Fork";
-  city[6-1] = Fork;
+  //E
+  node* Eugene = malloc(sizeof(node));
+  Eugene->name = "Eugene";
+  city[5-1] = Eugene;
 
+  //F
+  node* FairfaxCounty = malloc(sizeof(node));
+  FairfaxCounty->name = "Fairfax County";
+  city[6-1] = FairfaxCounty;
 
+  //G
+  node* Grapevine = malloc(sizeof(node));
+  Grapevine->name = "Grapevine";
+  city[7-1] = Grapevine;
+  
+  //H
+  node* Honolulu = malloc(sizeof(node));
+  Honolulu->name = "Honolulu";
+  city[8-1] = Honolulu;
 
+  //I
+  node* Indianapolis = malloc(sizeof(node));
+  Indianapolis->name = "Indianapolis";
+  city[9-1] = Indianapolis;
+  
+  //J
+  node* Juiter = malloc(sizeof(node));
+  Juiter->name = "Juiter";
+  city[10-1] = Juiter;
 
+  //K
+  node* KeyWest = malloc(sizeof(node));
+  KeyWest->name = "Key West";
+  city[11-1] = KeyWest;
 
+  //L
+  node* LosAngeles = malloc(sizeof(node));
+  LosAngeles->name = "Los Angeles";
+  city[12-1] = LosAngeles;
 
+  //M
+  node* Manhattan = malloc(sizeof(node));
+  Manhattan->name = "Manhattan";
+  city[13-1] = Manhattan;
+
+  //N
+  node* NewPort = malloc(sizeof(node));
+  NewPort->name = "New Port";
+  city[14-1] = NewPort;
+
+  //P
+  node* Philadelphia = malloc(sizeof(node));
+  Philadelphia->name = "Philadelphia";
+  city[15-1] = Philadelphia;
+
+  //O
+  node* Orlando = malloc(sizeof(node));
+  Orlando->name = "Orlando";
+  city[16-1] = Orlando;
+
+  //Q
+  node* QueenCity = malloc(sizeof(node));
+  QueenCity->name = "Queen City";
+  city[17-1] = QueenCity;
+
+  //R
+  node* Richmond = malloc(sizeof(node));
+  Richmond->name = "Richmond";
+  city[18-1] = Richmond;
+
+  //S
+  node* Seattle = malloc(sizeof(node));
+  Seattle->name = "Seattle";
+  city[19-1] = Seattle;
+
+  //T
+  node* TraverseCity = malloc(sizeof(node));
+  TraverseCity->name = "Traverse City";
+  city[20-1] = TraverseCity;
+
+  //U
+  node* Utiea = malloc(sizeof(node));
+  Utiea->name = "Utiea";
+  city[21-1] = Utiea;
+
+  //V
+  node* Vegas = malloc(sizeof(node));
+  Vegas->name = "Vegas";
+  city[22-1] = Vegas;
+
+  //W
+  node* Waikoloa = malloc(sizeof(node));
+  Waikoloa->name = "Waikoloa";
+  city[23-1] = Waikoloa;
+
+  //X
+  node* Xenia = malloc(sizeof(node));
+  Xenia->name = "Xenia";
+  city[24-1] = Xenia;
+
+  //Y
+  node* Yosemite = malloc(sizeof(node));
+  Yosemite->name = "Yosemite";
+  city[25-1] = Yosemite;
+
+  //Z
+  node* Zion = malloc(sizeof(node));
+  Zion->name = "Zion";
+  city[26-1] = Zion;
+
+////////////////////////////////////////
+
+   for(int i = 0; i < 26; i++){
+     for(node* list = city[i]; list != NULL; list = list->p){
+       printf("%s\n", list->name);
+     }
+   }
+
+//link: https://kidsareatrip.com/see-usa-cities-z/
 
 
   return 0;
